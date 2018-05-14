e04b69c50f3237474b3cdef078ea652ec3ca3a3e
Diego <dochoa743@gmail.com>
2018-05-14 17:49:07 -0500

    Primer Commit

---
+++ src/app/pages/prosgress/prosgress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prosgress',
+  templateUrl: './prosgress.component.html',
+  styles: []
+})
+export class ProsgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
